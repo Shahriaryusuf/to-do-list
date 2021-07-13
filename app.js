a747e763df9511bc1ae6d233628dcc0715ecada9
Shahriar Yusuf <1100shahriar1997@gmail.com>
2021-07-13 14:34:00 +0600

    second commit

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const app = express(); 
 app.set("view engine", "ejs")
 app.use(express.static("public"));
+
+
 app.use(bodyParser.urlencoded({extended:true}));
 
 let items = ["Buy Food","Eat Food","Digest Food"];
